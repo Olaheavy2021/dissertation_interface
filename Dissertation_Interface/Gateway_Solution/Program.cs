43530541819ba875922e1bce27b45eded2bdfd1d
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2023-09-03 12:57:35 +0100

    Setup Gateway for the project

--- Dissertation_Interface/Gateway_Solution/Program.cs
+++ Dissertation_Interface/Gateway_Solution/Program.cs
@@ -1,6 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+}
+builder.Services.AddOcelot(builder.Configuration);
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
