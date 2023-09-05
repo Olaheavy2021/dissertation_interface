44edeee5fb09404781af43427e0b86b573c42566
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2023-09-05 18:00:59 +0100

    Fix the docker running env bug

--- Dissertation_Interface/Gateway_Solution/Program.cs
+++ Dissertation_Interface/Gateway_Solution/Program.cs
@@ -9,7 +9,7 @@ if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"
 {
     builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
 }
-else if (isRunningInDocker.ToLower().Equals("true"))
+else if (!string.IsNullOrEmpty(isRunningInDocker) && isRunningInDocker.ToLower().Equals("true"))
 {
     builder.Configuration.AddJsonFile("ocelot.Docker.json", optional: false, reloadOnChange: true);
 }
